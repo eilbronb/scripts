7e84e0dd6b9c0cfb8f7deb47115f735bdfb0cdb8
Eilbron <ilbron@yahoo.com>
2018-10-31 22:05:01 -0700

    first

---
+++ ios_merger.py
@@ -0,0 +1,12 @@
+import napalm
+
+device_ip = 10.10.10.26
+username = 'admin'
+password = 'password123'
+
+napalm_driver = napalm.get_network_driver('ios')
+device = napalm_driver(hostname=device_ip,
+                       username=username,
+                       password=password)
+
+device.open()
